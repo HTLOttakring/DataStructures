9987e972091cb588db290e09c71af01bebf79fdf
ariegler <arnold.riegler@htl-ottakring.ac.at>
2015-09-13 20:57:56 +0200

    List

---
+++ ListDataStructure/List.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDataStructure
+{
+    public class List
+    {
+        ListItem head = null;
+
+
+        public ListItem GetHead()
+        {
+            return head;
+        }
+
+
+        public void Add(ListItem item)
+        {
+            if (head == null)
+            {
+                head = item;
+            }
+            else
+            {
+                ListItem current = GetLast();
+                current.Next = item;
+            }
+        }
+
+        private ListItem GetLast()
+        {
+            
+            if (head == null)
+                return null;
+            ListItem last = head;
+            while (last.Next != null)
+            {
+                last = last.Next;
+            }
+
+            return last;
+        }
+    }
+}
---
+++ ListDataStructure/ListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDataStructure
+{
+    public class ListItem
+    {
+        public string Value { get; set; }
+
+        public ListItem Next { get; set; }
+    }
+}
